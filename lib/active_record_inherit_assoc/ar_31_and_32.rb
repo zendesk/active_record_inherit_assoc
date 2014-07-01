9a75e938ab076ac30eebe5d2b3004710f7a82c53
Ben Osheroff <ben@zendesk.com>
2014-07-01 09:19:52 -0700

    whitespace

--- lib/active_record_inherit_assoc/ar_31_and_32.rb
+++ lib/active_record_inherit_assoc/ar_31_and_32.rb
@@ -3,4 +3,3 @@ require 'active_record'
 ActiveRecord::Associations::Builder::HasMany.valid_options   << :inherit
 ActiveRecord::Associations::Builder::HasOne.valid_options    << :inherit
 ActiveRecord::Associations::Builder::BelongsTo.valid_options << :inherit
-
--- lib/active_record_inherit_assoc/ar_4.rb
+++ lib/active_record_inherit_assoc/ar_4.rb
@@ -2,4 +2,3 @@ require 'active_record'
 
 # sorta hacky, but whatever.
 ActiveRecord::Associations::Builder::Association.valid_options << :inherit
-
