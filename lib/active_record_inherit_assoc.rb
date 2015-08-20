290a288bc24dc5fcb3d16c2772dcebc76c013871
Henry Hsu <hhsu@zendesk.com>
2015-08-20 14:19:44 -0700

    Review fixes.

--- lib/active_record_inherit_assoc.rb
+++ lib/active_record_inherit_assoc.rb
@@ -25,45 +25,28 @@ module ActiveRecordInheritAssocPrepend
   end
 end
 
-ActiveRecord::Associations::Association.class_eval do
-  prepend ActiveRecordInheritAssocPrepend
-end
+ActiveRecord::Associations::Association.send(:prepend, ActiveRecordInheritAssocPrepend)
 
 module ActiveRecordInheritPreloadAssocPrepend
-  def self.prepended(mod)
-    mod.class_eval do
-      alias_method_chain :associated_records_by_owner, :inherit
+  def associated_records_by_owner(*args)
+    super.tap do |result|
+      next unless reflection.options[:inherit]
+      result.each do |owner, associated_records|
+        filter_associated_records_with_inherit!(owner, associated_records)
+      end
     end
   end
 
   def filter_associated_records_with_inherit!(owner, associated_records)
-    return unless reflection.options[:inherit]
-
     associated_records.select! do |record|
-      Array(reflection.options[:inherit]).all? do |obj|
-        record.send(obj) == owner.send(obj)
-      end
-    end
-  end
-
-  if ActiveRecord::VERSION::MAJOR >= 4 && ActiveRecord::VERSION::MINOR >= 1
-    def associated_records_by_owner_with_inherit(preloader)
-      associated_records_by_owner_without_inherit(preloader).tap do |result|
-        result.each(&method(:filter_associated_records_with_inherit!)) if reflection.options[:inherit]
-      end
-    end
-  else
-    def associated_records_by_owner_with_inherit
-      associated_records_by_owner_without_inherit.tap do |result|
-        result.each(&method(:filter_associated_records_with_inherit!)) if reflection.options[:inherit]
+      Array(reflection.options[:inherit]).all? do |method_name|
+        record.send(method_name) == owner.send(method_name)
       end
     end
   end
 end
 
-ActiveRecord::Associations::Preloader::Association.class_eval do
-  prepend ActiveRecordInheritPreloadAssocPrepend
-end
+ActiveRecord::Associations::Preloader::Association.send(:prepend, ActiveRecordInheritPreloadAssocPrepend)
 
 class ActiveRecord::Base
   # Makes the model inherit the specified attribute from a named association.
